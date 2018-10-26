59c5fcaf0784623f0ada0b1d702e4d88c060327f
jolle <jolle@users.noreply.github.com>
2018-10-26 22:52:53 +0300

    ✨ Do not install devDependencies for production builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,14 +4,14 @@ rm -rf build
 echo "Building..."
 ./node_modules/.bin/parcel build --no-source-maps --out-dir=build --public-url "./" -t electron src/index.html --no-cache
 
-echo "Copying node_modules..."
-cp -R node_modules build
-
 echo "Copying electron..."
 cp src/electron.js build/index.js
 
 echo "Copying package.json..."
 cp src/package.json build/package.json
+
+echo "Installing node modules..."
+NODE_ENV=production yarn install --production --modules-folder=build/node_modules
 
 echo "Replacing DEV paths for PROD..."
 if [[ "$OSTYPE" == "darwin"* ]]; then
