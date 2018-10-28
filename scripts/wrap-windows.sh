ab9747b5569a062a74509e7793fdf094d5c3ede3
jolle <jolle@users.noreply.github.com>
2018-10-28 12:52:21 +0200

    🐛 Use .bat instead of .sh for Windows

--- scripts/wrap-windows.sh
+++
@@ -1,7 +0,0 @@
-echo "Packaging..."
-./node_modules/.bin/electron-packager build Livre --platform=win32 --overwrite --out=publications
-
-echo "Making Windows installer..."
-./node_modules/.bin/electron-installer-windows --src publications/Livre-win32-x64/ --dest publications/installers/
-
-echo "All done!"
