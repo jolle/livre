6c3c7eb4ace85385b5a678822944f5c55a6d9408
jolle <jolle@users.noreply.github.com>
2018-10-31 16:31:57 +0200

    🐛 Fix toolbar menu not appearing

--- src/electron.js
+++ src/electron.js
@@ -1,41 +1,21 @@
-// Modules to control application life and create native browser window
 const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+
 let mainWindow;
 
 const template = [
     {
-        label: 'Application',
-        submenu: [
-            {
-                label: 'Quit',
-                accelerator: 'Command+Q',
-                click: function() {
-                    app.quit();
-                }
-            }
-        ]
-    },
-    {
         label: 'Edit',
         submenu: [
-            { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
-            {
-                label: 'Redo',
-                accelerator: 'Shift+CmdOrCtrl+Z',
-                selector: 'redo:'
-            },
+            { role: 'undo' },
+            { role: 'redo' },
             { type: 'separator' },
-            { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
-            { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
-            { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
-            {
-                label: 'Select All',
-                accelerator: 'CmdOrCtrl+A',
-                selector: 'selectAll:'
-            }
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'pasteandmatchstyle' },
+            { role: 'delete' },
+            { role: 'selectall' }
         ]
     },
     {
@@ -51,13 +31,32 @@ const template = [
             { type: 'separator' },
             { role: 'togglefullscreen' }
         ]
+    },
+    {
+        role: 'window',
+        submenu: [{ role: 'minimize' }, { role: 'close' }]
     }
 ];
 
-Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+if (process.platform === 'darwin') {
+    template.unshift({
+        label: app.getName(),
+        submenu: [{ role: 'quit' }]
+    });
+
+    // Window menu
+    template[3].submenu = [
+        { role: 'close' },
+        { role: 'minimize' },
+        { role: 'zoom' },
+        { type: 'separator' },
+        { role: 'front' }
+    ];
+}
 
 function createWindow() {
-    // Create the browser window.
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
     mainWindow = new BrowserWindow({
         width: 1000,
         height: 800,
@@ -67,7 +66,6 @@ function createWindow() {
         }
     });
 
-    // and load the index.html of the app.
     mainWindow.loadFile(
         path.join(
             __dirname,
@@ -75,39 +73,23 @@ function createWindow() {
         )
     );
 
-    // Open the DevTools.
     //mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
     mainWindow.on('closed', function() {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null;
     });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function() {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
 app.on('activate', function() {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow();
     }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
